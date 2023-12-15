bd65892ea5996a41f4d13f98a12428bad34ec431
ahmed-my <yahaya.mymohammed@gmail.com>
2023-12-15 08:30:22 +0000

    Fix my code challenge

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -48,13 +48,13 @@ class User():
         - `False` if `pwd` is `None`
         - `False` if `pwd` is not a string
         - `False` if `__password` is `None`
-        - Compare `password` and the MD5 value of `pwd`
+        - Compare `__password` and the MD5 value of `pwd`
         """
         if pwd is None or type(pwd) is not str:
             return False
         if self.password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.password
 
 
 if __name__ == '__main__':
